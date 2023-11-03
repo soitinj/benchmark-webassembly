a866b0a1f49248747f8157ee6adeae992988f161
Jaakko Soitinaho <jaakko.soitinaho@veikkaus.fi>
2023-11-03 22:55:55 +0200

    Time execution

--- src/components/CalcForm.tsx
+++ src/components/CalcForm.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import BenchmarkDropdown from "./BenchmarkDropdown";
 
 const CalcForm = () => {
+
+    const [runtime, setRuntime] = useState(0.0);
+
+    const runCalcs = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const start = performance.now();
+        // run calculations here based on form values
+        const time = performance.now() - start;
+        setRuntime(Math.round(time * 1000));
+    }
+    
     return (
         <React.Fragment>
-            <form className='calc-form'>
+            <form className='calc-form' onSubmit={runCalcs} >
                 <BenchmarkDropdown calcName='approximate Pi'></BenchmarkDropdown>
                 <BenchmarkDropdown calcName='approximate Pi'></BenchmarkDropdown>
                 <BenchmarkDropdown calcName='approximate Pi'></BenchmarkDropdown>
@@ -12,6 +24,7 @@ const CalcForm = () => {
                     Run calculations
                 </button>
             </form>
+            <h1 className="text-light">Time: {runtime} ms</h1>
         </React.Fragment>
     )
 };
